8402aa032100a82c1de1346938bf95d688641c09
PathumLD <89245419+PathumLD@users.noreply.github.com>
2023-08-09 11:27:08 +0530

    Delete tritech-crm-main directory

--- tritech-crm-main/tritech-crm/config/login-check.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-    if(!isset($_SESSION['user'])) 
-    {
-        $_SESSION['no-login-message'] = "<div class='login-alert-failed' text-center'>Please login to get to access TriTech CRM</div>";
-        header('location: http://localhost/tritech-crm/');
-    }
-
-?>
--- tritech-crm-main/tritech-crm/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    include('config/constants.php');
-    session_destroy(); 
-    header('location: http://localhost/tritech-crm/');
-
-?>
-
